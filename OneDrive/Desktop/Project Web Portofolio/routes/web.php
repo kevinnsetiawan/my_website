6ae711737141854bffca3e719023d58fab29594b
Kevin Setiawan <159034180+kevinnsetiawan@users.noreply.github.com>
2026-06-23 23:21:08 +0700

    Delete OneDrive/Desktop/Project Web Portofolio directory

--- OneDrive/Desktop/Project Web Portofolio/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\ProjectController;
-
-Route::get('/', [ProjectController::class, 'home'])->name('home');
-Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
